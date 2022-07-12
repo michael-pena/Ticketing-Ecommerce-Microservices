4659c7be4f889b5d6e9dc02dbc3a1ceb5a1b1fd9
michael-pena <michael.pena@protonmail.ch>
2022-07-11 20:35:57 -0500

    added the nats streaming server

--- tickets/src/routes/__test__/update.test.ts
+++ tickets/src/routes/__test__/update.test.ts
@@ -13,16 +13,67 @@ it("returns a 404 ticket id does not exist ", async () => {
 });
 
 it("returns a 401 if the user is not authenticated", async () => {
-    const id = new mongoose.Types.ObjectId().toHexString();
+  const id = new mongoose.Types.ObjectId().toHexString();
 
   await request(app)
-    .put(`/api/tickets/${id}`)    
+    .put(`/api/tickets/${id}`)
     .send({ title: "asdf", price: 20 })
     .expect(401);
 });
 
-it("returns a 401 if a user does not own a ticket", async () => {});
+it("returns a 401 if a user does not own a ticket", async () => {
+  const response = await request(app)
+    .post(`/api/tickets/`)
+    .set("Cookie", global.signin())
+    .send({ title: "asdf", price: 20 });
 
-it("returns a 400 if user provides an invalid title or price", async () => {});
+  await request(app)
+    .put(`/api/tickets/${response.body.id}`)
+    .set("Cookie", global.signin())
+    .send({
+      title: "asdsa",
+      price: 1000,
+    })
+    .expect(401);
+});
 
-it("it updates the ticket provided valid inputs", async () => {});
+it("returns a 400 if user provides an invalid title or price", async () => {
+  const cookie = global.signin();
+  const response = await request(app)
+    .post(`/api/tickets/`)
+    .set("Cookie", cookie)
+    .send({ title: "asdf", price: 20 });
+
+  await request(app)
+    .put(`/api/tickets/${response.body.id}`)
+    .set("Cookie", cookie)
+    .send({ title: "", price: 20 })
+    .expect(400);
+
+  await request(app)
+    .put(`/api/tickets/${response.body.id}`)
+    .set("Cookie", cookie)
+    .send({ title: "aasd", price: -10 })
+    .expect(400);
+});
+
+it("it updates the ticket provided valid inputs", async () => {
+  const cookie = global.signin();
+  const response = await request(app)
+    .post(`/api/tickets/`)
+    .set("Cookie", cookie)
+    .send({ title: "asdf", price: 20 });
+
+    await request(app)
+    .put(`/api/tickets/${response.body.id}`)
+    .set("Cookie", cookie)
+    .send({ title: "new title", price: 100 })
+    .expect(200);
+
+    const ticketResponse = await request(app)
+    .get(`/api/tickets/${response.body.id}`)
+    .send();
+
+    expect(ticketResponse.body.title).toEqual('new title');
+    expect(ticketResponse.body.price).toEqual(100);
+});
--- tickets/src/routes/update.ts
+++ tickets/src/routes/update.ts
@@ -11,6 +11,34 @@ import { Ticket } from "../models/ticket";
 
 const router = express.Router();
 
-router.put("/api/tickets/:id", async (req: Request, res: Response) => {});
+router.put(
+  "/api/tickets/:id",
+  requireAuth,
+  [
+    body("title").not().isEmpty().withMessage("Title is required"),
+    body("price").isFloat({ gt: 0 }).withMessage("Title is required"),
+  ],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    const ticket = await Ticket.findById(req.params.id);
+
+    if (!ticket) {
+      throw new NotFoundError();
+    }
+
+    if (ticket.userId !== req.currentUser!.id) {
+      throw new NotAuthorizedError();
+    }
+
+    ticket.set({
+        title: req.body.title,
+        price: req.body.price
+    });
+
+    await ticket.save();
+
+    return res.send(ticket);
+  }
+);
 
 export { router as updateTicketRouter };
