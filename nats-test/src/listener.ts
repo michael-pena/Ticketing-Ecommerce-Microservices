8bc6f8835b8cce12b58c6af47a62ba67e931a71d
michael-pena <michael.pena@protonmail.ch>
2022-07-16 14:29:50 -0500

    added nats-testing project

---
+++ nats-test/src/listener.ts
@@ -0,0 +1,13 @@
+import nats from "node-nats-streaming";
+
+console.clear();
+
+const stan = nats.connect("ticketing", "123", {
+  url: "http:localhost:4222",
+});
+
+stan.on("connect", () => {
+  console.log("Listener connected to NATS");
+
+  const subscription = stan.subscribe('ticket:created');
+});
