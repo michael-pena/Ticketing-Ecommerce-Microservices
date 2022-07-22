07c48f53469a4b53898d47503a99f875742e60af
michael-pena <michael.pena@protonmail.ch>
2022-07-21 19:37:49 -0500

    added the TicketCreatedListener class

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -4,7 +4,7 @@ import { randomBytes } from "crypto";
 console.clear();
 
 const stan = nats.connect("ticketing", randomBytes(4).toString("hex"), {
-  url: "http:localhost:4222",
+  url: "http://localhost:4222",
 });
 
 stan.on("connect", () => {
@@ -15,27 +15,7 @@ stan.on("connect", () => {
     process.exit();
   });
 
-  const options = stan
-    .subscriptionOptions()
-    .setManualAckMode(true)
-    .setDeliverAllAvailable()
-    .setDurableName("accounting-service");
-
-  const subscription = stan.subscribe(
-    "ticket:created",
-    "orders-service-queue-group",
-    options
-  );
-
-  subscription.on("message", (msg: Message) => {
-    const data = msg.getData();
-
-    if (typeof data === "string") {
-      console.log(`Recieved event #${msg.getSequence()}, with data: ${data}`);
-    }
-
-    msg.ack();
-  });
+  new TicketCreatedListener(stan).listen();
 });
 
 process.on("SIGINT", () => stan.close());
@@ -44,9 +24,9 @@ process.on("SIGTERM", () => stan.close());
 abstract class Listener {
   abstract subject: string;
   abstract queueGroupName: string;
-  private client: Stan;
   abstract onMessage(data: any, msg: Message): void;
-  protected ackWait = 5 * 100;
+  private client: Stan;
+  protected ackWait = 5 * 1000;
 
   constructor(client: Stan) {
     this.client = client;
@@ -69,19 +49,29 @@ abstract class Listener {
     );
 
     subscription.on("message", (msg: Message) => {
-      console.log(`Message recieved ${this.subject} / ${this.queueGroupName}`);
+      console.log(`Message received: ${this.subject} / ${this.queueGroupName}`);
 
       const parsedData = this.parseMessage(msg);
-      
       this.onMessage(parsedData, msg);
     });
   }
 
   parseMessage(msg: Message) {
     const data = msg.getData();
-
     return typeof data === "string"
       ? JSON.parse(data)
-      : JSON.parse(data.toString("utf-8"));
+      : JSON.parse(data.toString("utf8"));
   }
 }
+
+class TicketCreatedListener extends Listener {
+  subject = "ticket:created";
+  queueGroupName = "payments-service";
+
+  onMessage(data: any, msg: Message) {
+    console.log("Event data!", data);
+
+    msg.ack();
+  }
+}
+
