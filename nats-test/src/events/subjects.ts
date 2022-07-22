710dc086906e055a9d02c1b3dafb7399c994c28f
michael-pena <michael.pena@protonmail.ch>
2022-07-21 21:20:39 -0500

    added the ticket created event data coupling for Listener class

---
+++ nats-test/src/events/subjects.ts
@@ -0,0 +1,10 @@
+export enum Subjects {
+    TicketCreated = 'ticket:created',
+    OrderUpdated = 'order:updated'
+}
+
+const printSubject = (subject: Subjects) => {
+
+};
+
+printSubject(Subjects.OrderUpdated);
