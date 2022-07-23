1f9303ff07dc228907329f6c2c8cfbd64d65c3a1
michael-pena <michael.pena@protonmail.ch>
2022-07-23 13:08:34 -0500

    added the publisher base class

--- nats-test/src/events/ticket-created-listener.ts
+++ nats-test/src/events/ticket-created-listener.ts
@@ -7,9 +7,12 @@ export class TicketCreatedListener extends Listener<TicketCreatedEvent> {
   subject: Subjects.TicketCreated = Subjects.TicketCreated;
   queueGroupName = "payments-service";
 
-  onMessage(data: any, msg: Message) {
+  onMessage(data: TicketCreatedEvent['data'], msg: Message) {
     console.log("Event data!", data);
 
+    console.log(data.id);
+    console.log(data.title);
+    console.log(data.price);
     msg.ack();
   }
 }
