a5ff5016cc71e125e1ae212afd9fe2e611077073
michael-pena <michael.pena@protonmail.ch>
2022-07-30 18:24:37 -0500

    fixed the ticket ingress and updated the new ticket route

--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -1,20 +1,20 @@
-import express, { Request, Response } from "express";
-import { body } from "express-validator";
-import { requireAuth, validateRequest } from "@mpena/common";
-import { Ticket } from "../models/ticket";
-import { TicketCreatedPublisher } from "../events/publishers/ticket-created-publisher";
+import express, { Request, Response } from 'express';
+import { body } from 'express-validator';
+import { requireAuth, validateRequest } from '@mpena/common';
+import { Ticket } from '../models/ticket';
+import { TicketCreatedPublisher } from '../events/publishers/ticket-created-publisher';
+import { natsWrapper } from '../nats-wrapper';
 
 const router = express.Router();
 
 router.post(
-  "/api/tickets",
+  '/api/tickets',
   requireAuth,
   [
-    body("title").not().isEmpty().withMessage("Title is required"),
-
-    body("price")
+    body('title').not().isEmpty().withMessage('Title is required'),
+    body('price')
       .isFloat({ gt: 0 })
-      .withMessage("Price must be greater than 0"),
+      .withMessage('Price must be greater than 0'),
   ],
   validateRequest,
   async (req: Request, res: Response) => {
@@ -25,9 +25,8 @@ router.post(
       price,
       userId: req.currentUser!.id,
     });
-
     await ticket.save();
-    await new TicketCreatedPublisher(client).publish({
+    await new TicketCreatedPublisher(natsWrapper.client).publish({
       id: ticket.id,
       title: ticket.title,
       price: ticket.price,
