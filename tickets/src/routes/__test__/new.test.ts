fc15c30df466145865e0c69b97acc0be83e93ff8
michael-pena <michael.pena@protonmail.ch>
2022-07-31 20:43:19 -0500

    added more test cases to jest

--- tickets/src/routes/__test__/new.test.ts
+++ tickets/src/routes/__test__/new.test.ts
@@ -1,7 +1,7 @@
 import request from "supertest";
 import { app } from "../../app";
 import { Ticket } from "../../models/ticket";
-
+import { natsWrapper } from "../../nats-wrapper";
 
 it("has a router handler listening to /api/tickets for post requests", async () => {
   const response = await request(app).post("/api/tickets").send({});
@@ -71,9 +71,22 @@ it("creates a ticket with valid inputs", async () => {
     })
     .expect(201);
 
-    //check if record was added
-    tickets = await Ticket.find({});
-    expect(tickets.length).toEqual(1);
+  //check if record was added
+  tickets = await Ticket.find({});
+  expect(tickets.length).toEqual(1);
+});
 
-    
+
+it("publishes an event", async () => {
+
+  await request(app)
+    .post("/api/tickets")
+    .set("Cookie", global.signin())
+    .send({
+      title: "asldkfj",
+      price: 20,
+    })
+    .expect(201);
+
+    expect(natsWrapper.client.publish).toHaveBeenCalled();
 });
--- tickets/src/routes/__test__/update.test.ts
+++ tickets/src/routes/__test__/update.test.ts
@@ -1,7 +1,7 @@
 import request from "supertest";
 import { app } from "../../app";
 import mongoose, { mongo } from "mongoose";
-
+import { natsWrapper } from "../../nats-wrapper";
 
 it("returns a 404 ticket id does not exist ", async () => {
   const id = new mongoose.Types.ObjectId().toHexString();
@@ -65,16 +65,32 @@ it("it updates the ticket provided valid inputs", async () => {
     .set("Cookie", cookie)
     .send({ title: "asdf", price: 20 });
 
-    await request(app)
+  await request(app)
     .put(`/api/tickets/${response.body.id}`)
     .set("Cookie", cookie)
     .send({ title: "new title", price: 100 })
     .expect(200);
 
-    const ticketResponse = await request(app)
+  const ticketResponse = await request(app)
     .get(`/api/tickets/${response.body.id}`)
     .send();
 
-    expect(ticketResponse.body.title).toEqual('new title');
-    expect(ticketResponse.body.price).toEqual(100);
+  expect(ticketResponse.body.title).toEqual("new title");
+  expect(ticketResponse.body.price).toEqual(100);
+});
+
+it("publishes an event", async () => {
+  const cookie = global.signin();
+  const response = await request(app)
+    .post(`/api/tickets/`)
+    .set("Cookie", cookie)
+    .send({ title: "asdf", price: 20 });
+
+  await request(app)
+    .put(`/api/tickets/${response.body.id}`)
+    .set("Cookie", cookie)
+    .send({ title: "new title", price: 100 })
+    .expect(200);
+
+  expect(natsWrapper.client.publish).toHaveBeenCalled();
 });
