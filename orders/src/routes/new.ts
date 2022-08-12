17772b0c9a0ac035582fea6f7e3be3fcb194dddd
michael-pena <michael.pena@protonmail.ch>
2022-08-11 21:18:59 -0500

    updated the new route for orders service

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -1,8 +1,26 @@
 import express, { Request, Response } from "express";
+import { requireAuth, validateRequest } from "@mpena/common";
+import { body } from "express-validator";
+import mongoose from "mongoose";
+
 const router = express.Router();
 
-router.post("/api/orders", async (req: Request, res: Response) => {
-  res.send({});
-});
+router.post(
+  "/api/orders",
+  requireAuth,
+  [
+    body("ticketId")
+      .not()
+      .isEmpty()
+      .custom((input: string) => {
+        mongoose.Types.ObjectId.isValid(input);
+      })
+      .withMessage("TicketId Must be provided"),
+  ],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    res.send({});
+  }
+);
 
 export { router as newOrderRouter };
