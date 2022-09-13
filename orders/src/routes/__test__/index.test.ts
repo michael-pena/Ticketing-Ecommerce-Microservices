540a2b18682e3133e835462f9bbc2dc56f445ee7
michael-pena <michael.pena@protonmail.ch>
2022-09-12 21:07:51 -0500

    added the test case to retrieve user orders for order service

---
+++ orders/src/routes/__test__/index.test.ts
@@ -0,0 +1,60 @@
+import request from "supertest";
+import { app } from "../../app";
+import { Order } from "../../models/order";
+import { Ticket } from "../../models/ticket";
+
+const buildTicket = async () => {
+  const ticket = Ticket.build({
+    title: "concert",
+    price: 20,
+  });
+
+  await ticket.save();
+
+  return ticket;
+};
+
+it("fetches orders for a particular user", async () => {
+  //create three tickets
+  const ticketOne = await buildTicket();
+  const ticketTwo = await buildTicket();
+  const ticketThree = await buildTicket();
+
+  // create two different users
+  const userOne = global.signin();
+  const userTwo = global.signin();
+
+  // create on order as user #1 - use the userone cookie
+  await request(app)
+    .post("/api/orders")
+    .set("Cookie", userOne)
+    .send({ ticketId: ticketOne.id })
+    .expect(201);
+
+  // create two orders as user #2
+  const { body: orderOne } = await request(app)
+    .post("/api/orders")
+    .set("Cookie", userTwo)
+    .send({ ticketId: ticketTwo.id })
+    .expect(201);
+
+    const { body: orderTwo } = await request(app)
+    .post("/api/orders")
+    .set("Cookie", userTwo)
+    .send({ ticketId: ticketThree.id })
+    .expect(201);
+
+  // make request to get orders from user #2
+  const response = await request(app)
+    .get("/api/orders")
+    .set("Cookie", userTwo)
+    .expect(200);
+
+  // make sure we only got the orders from user #2
+  expect(response.body.orders.length).toEqual(2);
+
+  //check we have the two correct orders for user 2
+  expect(response.body.orders[0].id).toEqual(orderOne.id);
+  expect(response.body.orders[1].id).toEqual(orderTwo.id);
+
+});
