1b68a35ec19ad06bbcfd30bf34d8b7dd5024bcb2
michael-pena <michael.pena@protonmail.ch>
2022-09-19 20:57:44 -0500

    updated show order route in orders service

--- orders/src/routes/show.ts
+++ orders/src/routes/show.ts
@@ -1,8 +1,23 @@
 import express, { Request, Response } from "express";
+import { NotAuthorizedError, NotFoundError, requireAuth } from "@mpena/common";
+import { Order } from "../models/order";
+
 const router = express.Router();
 
-router.get("/api/orders/:orderId", async (req: Request, res: Response) => {
-  res.send({});
+router.get("/api/orders/:orderId", requireAuth, async (req: Request, res: Response) => {
+  const order = await Order.findById(req.params.orderId).populate('ticket');
+
+  //order not found
+  if (!order) {
+    throw new NotFoundError();
+  }
+
+  //order user id doesn't equal user's id - not authorized to view
+  if (order.userId !== req.currentUser!.id) {
+    throw new NotAuthorizedError();
+  }
+
+  res.send(order);
 });
 
 export { router as showOrderRouter };
