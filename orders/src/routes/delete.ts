622f11d2dbf63d12d81261476ff05a5abc6946ec
michael-pena <michael.pena@protonmail.ch>
2022-09-24 18:47:52 -0500

    added the delete route and test for orders service

--- orders/src/routes/delete.ts
+++ orders/src/routes/delete.ts
@@ -4,23 +4,27 @@ import { Order, OrderStatus } from "../models/order";
 
 const router = express.Router();
 
-router.delete("/api/orders/:id", requireAuth, async (req: Request, res: Response) => {
-  const {orderId} = req.params; 
+router.delete(
+  "/api/orders/:orderId",
+  requireAuth,
+  async (req: Request, res: Response) => {
+    const { orderId } = req.params;
 
-  const order = await Order.findById(orderId);
+    const order = await Order.findById(orderId);
 
-  if (!order) {
-    throw new NotFoundError();
+    if (!order) {
+      throw new NotFoundError();
+    }
+    if (order.userId !== req.currentUser!.id) {
+      throw new NotAuthorizedError();
+    }
+    order.status = OrderStatus.Cancelled;
+    await order.save();
+
+    // publishing an event saying this was cancelled!
+
+    res.status(204).send(order);
   }
-
-  if (order.userId !== req.currentUser!.id) {
-    throw new NotAuthorizedError();
-  }
-
-  order.status = OrderStatus.Cancelled;
-  await order.save();
-
-  res.status(204).send({order});
-});
+);
 
 export { router as deleteOrderRouter };
