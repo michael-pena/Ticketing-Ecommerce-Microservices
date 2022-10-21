340deaa84ec0a44b1a0d2f6e875cbb10ca873ecd
michael-pena <michael.pena@protonmail.ch>
2022-10-20 20:59:40 -0500

    updated order model in order services

--- orders/src/models/order.ts
+++ orders/src/models/order.ts
@@ -1,3 +1,4 @@
+import { updateIfCurrentPlugin } from "mongoose-update-if-current";
 import mongoose, { mongo } from "mongoose";
 import { OrderStatus } from "@mpena/common";
 import {TicketDoc} from "./ticket";
@@ -52,6 +53,9 @@ const orderSchema = new mongoose.Schema(
     },
   }
 );
+
+orderSchema.set('versionKey', 'version');
+orderSchema.plugin(updateIfCurrentPlugin);
 
 orderSchema.statics.build = (attrs: OrderAttrs) => {
   return new Order(attrs);
