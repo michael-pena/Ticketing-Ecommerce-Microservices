098e7038b85fa0df5ff09d7ec9516333d0c94686
michael-pena <michael.pena@protonmail.ch>
2022-10-25 21:34:40 -0500

    setup for tickets service order created listener

---
+++ tickets/src/events/listeners/order-created-listener.ts
@@ -0,0 +1,13 @@
+import { Listener, OrderCreatedEvent, Subjects } from "@mpena/common";
+import { queueGroupName } from "./queue-group-name";
+import { Message } from "node-nats-streaming";
+
+export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
+    subject: Subjects.OrderCreated = Subjects.OrderCreated;
+
+    queueGroupName= queueGroupName;
+
+    async onMessage(data: OrderCreatedEvent['data'], msg: Message) {
+
+    }
+}
