0acc2c460fe51a6a343c1885fcd650e57d7ce3a5
michael-pena <michael.pena@protonmail.ch>
2022-11-02 20:53:40 -0500

    updated the order created listener tickets service and common module

--- tickets/src/events/listeners/order-created-listener.ts
+++ tickets/src/events/listeners/order-created-listener.ts
@@ -2,6 +2,7 @@ import { Listener, OrderCreatedEvent, Subjects } from "@mpena/common";
 import { queueGroupName } from "./queue-group-name";
 import { Message } from "node-nats-streaming";
 import { Ticket } from "../../models/ticket";
+import { TicketUpdatedPublisher } from "../publishers/ticket-updated-publisher";
 
 export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
   subject: Subjects.OrderCreated = Subjects.OrderCreated;
@@ -23,7 +24,16 @@ export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
     //save the ticket
     await ticket.save();
 
-    // ack the message
+    await new TicketUpdatedPublisher(this.client).publish({
+      id: ticket.id,
+      price: ticket.price,
+      title: ticket.title,
+      userId: ticket.userId,
+      orderId: ticket.orderId,
+      version: ticket.version,
+    });
+
+    // ack the message - emit event
     msg.ack();
   }
 }
